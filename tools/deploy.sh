41b8b4d75ff663286d1513e0169ecbde39735dc5
Tyler Veness <calcmogul@gmail.com>
2020-01-26 13:04:11 -0800

    Deploy .so files and contents of deploy folder with make.py

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -7,8 +7,24 @@ if [ $# -ne 1 ] ; then
     exit 1
 fi
 
+# Stop old robot program and download new one
 ssh lvuser@$HOST "rm -f /home/lvuser/frcUserProgram"
 ssh lvuser@$HOST "killall -q netconsole-host || :"
 scp $1/frcUserProgram lvuser@$HOST:/home/lvuser
 ssh admin@$HOST "setcap 'cap_sys_nice=pe' /home/lvuser/frcUserProgram"
+
+# Deploy .so files
+# TODO
+
+# Deploy contents of "deploy" directory
+ssh lvuser@$HOST "mkdir -p /home/lvuser/deploy"
+scp -r deploy/* lvuser@$HOST:/home/lvuser/deploy
+
+# As far as I can tell, the order of this doesn't matter. It only comments out
+# some stuff for the LabVIEW runtime that apparently isn't needed, and
+# dramatically reduces memory usage.
+# See https://github.com/wpilibsuite/EclipsePlugins/pull/154
+ssh admin@$HOST "sed -i -e 's/^StartupDLLs/;StartupDLLs/' /etc/natinst/share/ni-rt.ini"
+
+# Restart robot program
 ssh lvuser@$HOST ". /etc/profile.d/natinst-path.sh; chmod a+x /home/lvuser/frcUserProgram; /usr/local/frc/bin/frcKillRobot.sh -t -r;"
